3350a3933d2944ec7266cd5768247f6b1789a8d0
kcho2001 <kec623@lehigh.edu>
2022-11-01 12:24:53 -0400

    Added function to check every worker's unpaid days to determine paid or not

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
 import logo from "./logo.svg";
 import { GET_DAYS_UNPAID } from "./query";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { useQuery } from "@apollo/client";
 import { ColorRing } from "react-loader-spinner";
 import * as React from "react";
 import { Paid } from "./paid.js";
 import { Unpaid } from "./unpaid.js";
-import "./App.css";
+import useWindowDimensions from "./hooks/windowDimensions";
+// import { styles } from "./styles.js";
 
 function checkPaidStatus(workers) {
   for (let worker of workers) {
-    //Check unpaid status of each worker
+    // Assuming that if a worker hasn't been paid for more than 14 days
+    // The application should not return a verified paid page
+    if (worker.daysUnpaid >= 14) {
+      return false;
+    }
   }
+  return true;
 }
 
 function App() {
   const [paid, setPaid] = React.useState(false);
+  const { height, width } = useWindowDimensions();
 
   const { loading, error, data } = useQuery(GET_DAYS_UNPAID, {
-    onCompleted: () => {
+    onCompleted: (data) => {
       // setWorkers(data.checkIns);
-      //checkPaidStatus(workers);
-      console.log(data);
-      setPaid(true);
+      setPaid(checkPaidStatus(data.workers));
     },
   });
 
   return (
     <>
       {loading && (
-        <View>
+        <View styles={{ justifyContent: "center", alignItems: "center" }}>
           <ColorRing
             visible={true}
-            height="80"
-            width="80"
+            height={height / 2}
+            width={width}
             ariaLabel="blocks-loading"
             wrapperStyle={{}}
             wrapperClass="blocks-wrapper"
@@ -53,3 +58,55 @@ function App() {
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  screen: {
+    flex: "1",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 1,
+  },
+  mainText: {
+    fontSize: 20,
+    color: "black",
+    paddingBottom: 5,
+    paddingTop: 0,
+    marginTop: 0,
+  },
+  content: {
+    padding: 30,
+  },
+  list: {
+    marginTop: 0,
+  },
+  bottom: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: 20,
+  },
+  buttonStyle: {
+    backgroundColor: "#3399FF",
+    borderWidth: 0,
+    color: "#FFFFFF",
+    borderColor: "#3399FF",
+    height: 40,
+    alignItems: "center",
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  buttonTextStyle: {
+    color: "#FFFFFF",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
