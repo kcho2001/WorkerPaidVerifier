530dbb370aa49e59ed882f0cffeb3774e862a464
kcho2001 <kec623@lehigh.edu>
2022-11-10 10:54:03 -0500

    Added more verbose information regarding payment info

--- src/paid.js
+++ src/paid.js
@@ -1,9 +1,28 @@
-import { styles } from "./styles";
+import "./index.css";
 
-export function Paid() {
+export function Paid(props) {
   return (
-    <div>
-      <p>Paid</p>
+    <div className="center">
+      <div className="dashboard">
+        <p className="header">Workers have been paid!</p>
+        <div>
+          <a href="https://goerli.etherscan.io/address/0xD260302a241A2CFBc1493676b4fA3f365870De59">
+            Blockexplorer
+          </a>
+        </div>
+        <div>
+          <p className="text inline"> Total number of workers: </p>{" "}
+          <p className="text inline"> {props.workerLength} </p>
+        </div>
+        <div>
+          <p className="text inline">
+            Most days a worker hasn't been paid for work is:{" "}
+          </p>{" "}
+          <p className="text inline">{props.maxUnpaid} </p>{" "}
+          <p className="text inline"> days</p>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
