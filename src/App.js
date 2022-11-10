744e85043b81442d06afefc347fa33befdc6b1ad
kcho2001 <kec623@lehigh.edu>
2022-11-10 10:53:35 -0500

    Passing in parameters to other screens to display results from query

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import logo from "./logo.svg";
 import { GET_DAYS_UNPAID } from "./query";
-import { Text, View, StyleSheet } from "react-native";
 import { useQuery } from "@apollo/client";
 import { ColorRing } from "react-loader-spinner";
 import * as React from "react";
 import { Paid } from "./paid.js";
 import { Unpaid } from "./unpaid.js";
 import useWindowDimensions from "./hooks/windowDimensions";
-import { styles } from "./styles.js";
 
-function checkPaidStatus(workers) {
+function checkPaidStatus(workers, setMaxUnpaid) {
+  const maxUnpaid = 0;
+  const paidStatus = true;
   for (let worker of workers) {
     // Assuming that if a worker hasn't been paid for more than 14 days
     // The application should not return a verified paid page
+    console.log(worker.id);
+    if (worker.daysUnpaid >= maxUnpaid) {
+      console.log("greater" + maxUnpaid);
+      setMaxUnpaid(maxUnpaid);
+    }
     if (worker.daysUnpaid >= 14) {
-      return false;
+      paidStatus = false;
     }
   }
-  return true;
+  return paidStatus;
 }
 
 function App() {
   const [paid, setPaid] = React.useState(false);
+  const [maxUnpaid, setMaxUnpaid] = React.useState(0);
   const { height, width } = useWindowDimensions();
 
   const { loading, error, data } = useQuery(GET_DAYS_UNPAID, {
     onCompleted: (data) => {
       // setWorkers(data.checkIns);
-      setPaid(checkPaidStatus(data.workers));
+      setPaid(checkPaidStatus(data.workers, setMaxUnpaid));
     },
   });
 
@@ -51,8 +57,12 @@ function App() {
           <p>Error retrieving data: {error.message}</p>
         </div>
       )}
-      {!error && !loading && !paid && <Unpaid />}
-      {!error && !loading && paid && <Paid />}
+      {!error && !loading && !paid && (
+        <Unpaid maxUnpaid={maxUnpaid} workerLength={data.workers.length} />
+      )}
+      {!error && !loading && paid && (
+        <Paid maxUnpaid={maxUnpaid} workerLength={data.workers.length} />
+      )}
     </>
   );
 }
