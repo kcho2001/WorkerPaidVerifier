e6857b2b2227b7f05d14d67ffa6e107ed78f5688
kcho2001 <kec623@lehigh.edu>
2022-11-21 19:45:34 -0500

    Now displaying more information on paid screen, with proper formatting

--- src/paid.js
+++ src/paid.js
@@ -4,22 +4,27 @@ export function Paid(props) {
   return (
     <div className="center dashboard">
       <div className="content">
-        <p className="header">Workers have been paid!</p>
-        <div>
+        <p className="header"> Verification Dashboard</p>
+        <div style={{ justifyContent: "center", flexDirection: "column" }}>
           <a href="https://goerli.etherscan.io/address/0xD260302a241A2CFBc1493676b4fA3f365870De59">
-            Blockexplorer
+            <img src="photos/etherscan-logo-circle.png" className="iconImage" />
           </a>
+          <p style={{ margin: 0, fontSize: "small" }}>
+            Verify Information
+          </p>
         </div>
-        <div>
-          <p className="text inline"> Total number of workers: </p>{" "}
-          <p className="text inline"> {props.workerLength} </p>
-        </div>
-        <div>
+        <div className="contentRow">
           <p className="text inline">
-            Most days a worker hasn't been paid for work is:{" "}
-          </p>{" "}
-          <p className="text inline">{props.maxUnpaid} </p>{" "}
-          <p className="text inline"> days</p>
+            Workers have been paid last: {props.recentPayment.month}/{props.recentPayment.day}/{props.recentPayment.year}
+          </p>
+        </div>
+        <div className="contentRow">
+          <p className="text inline">
+            Most unpaid work days: &nbsp;{props.maxUnpaid} days
+          </p>
+        </div>
+        <div className="contentRow">
+          <p className="text inline"> Total number of workers:&nbsp; {props.workerLength}</p>
         </div>
       </div>
     </div>
